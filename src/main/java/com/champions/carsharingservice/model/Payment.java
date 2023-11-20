f0831794b53a54ab302ac7a8670c87e302103dd1
Yura Novak <yuranovakr@gmail.com>
2023-11-20 18:39:07 +0200

    add new model package

--- src/main/java/com/champions/carsharingservice/model/Payment.java
+++ src/main/java/com/champions/carsharingservice/model/Payment.java
@@ -1,4 +1,4 @@
-package model;
+package com.champions.carsharingservice.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- src/main/java/com/champions/carsharingservice/model/Rental.java
+++ src/main/java/com/champions/carsharingservice/model/Rental.java
@@ -1,4 +1,4 @@
-package model;
+package com.champions.carsharingservice.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
