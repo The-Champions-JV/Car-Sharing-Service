5c09c2374304f38ca2c316894e6cd05f7c681a9d
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-11-20 15:19:20 +0200

    [model] rental entity added

---
+++ src/main/java/com/champions/carsharingservice/model/User.java
@@ -0,0 +1,19 @@
+package com.champions.carsharingservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    private Set<Rental> rentals = new HashSet<>();
+}
