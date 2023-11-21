dcebe2d834265aab7d39d7a1c359966fe276b69a
Tetiana Denchyk <tanushka.den7@gmail.com>
2023-11-21 21:28:10 +0200

    added regex to email annotation

--- src/main/java/com/champions/carsharingservice/dto/UserRegistrationRequestDto.java
+++ src/main/java/com/champions/carsharingservice/dto/UserRegistrationRequestDto.java
@@ -4,8 +4,18 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
-public record UserRegistrationRequestDto(@NotBlank @Email @Length(max = 255) String email,
-                                         @NotBlank @Length(min = 8, max = 50) String password,
-                                         @NotBlank @Length(max = 50) String firstName,
-                                         @NotBlank @Length(max = 50) String lastName) {
+public record UserRegistrationRequestDto(
+        @NotBlank
+        @Email(regexp = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
+        @Length(max = 255)
+        String email,
+        @NotBlank
+        @Length(min = 8, max = 50)
+        String password,
+        @NotBlank
+        @Length(max = 50)
+        String firstName,
+        @NotBlank
+        @Length(max = 50)
+        String lastName) {
 }
